bd592fb7e7d8a3e5a14e0a86bfd7aa42cb5fa4ff
Roshith R <r.roshith@gmail.com>
2021-09-06 06:17:43 +0530

    function calls

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,9 @@
 const abc = 12345;
+let names = 'hey';
+//  = functions
+const getFullName = (firstName, lastName) => [
+    firstName,
+    lastName,
+];
+// ! 1 . Array[string], 2.  string[] || number[]
+console.log(getFullName('1', '2'));
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
 const abc = 12345
+let names: string = 'hey'
+
+//  = functions
+
+const getFullName = (firstName: string, lastName: string): Array<string> => [
+  firstName,
+  lastName,
+]
+
+
+
+// ! 1 . Array[string], 2.  string[] || number[]
+console.log(getFullName('1', '2'))
